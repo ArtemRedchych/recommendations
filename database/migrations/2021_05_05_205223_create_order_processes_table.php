d49e526fb1468bdc9cba354e41ccac762b1d9116
artemredchych <artemredchych@gmail.com>
2021-05-06 18:49:36 +0200

    database foreign key removed

--- database/migrations/2021_05_05_205223_create_order_processes_table.php
+++ database/migrations/2021_05_05_205223_create_order_processes_table.php
@@ -19,8 +19,8 @@ class CreateOrderProcessesTable extends Migration
             $table->string('created',20)->nullable();
             $table->double('final_price', 15, 8)->nullable()->default(0);
 
-            $table->foreign('id_customers')->references('id')->on('custommers');
-            
+            //$table->foreign('id_customers')->references('id')->on('custommers');
+            $table->dropForeign('id_customers');
             $table->primary('number_order');
         });
     }
@@ -35,3 +35,8 @@ class CreateOrderProcessesTable extends Migration
         Schema::dropIfExists('order_processes');
     }
 }
+
+SELECT *
+  FROM information_schema.columns
+ WHERE  table_name   = 'products'
+     ;
